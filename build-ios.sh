084d450fa86c2f62be4ff750f9e54fa3840b0cbc
Shawn Presser <shawnpresser@gmail.com>
2019-05-01 19:50:51 -0500

    Fix build script

--- build-ios.sh
+++ build-ios.sh
@@ -1,5 +1,22 @@
-cd src
 set -x
+set -e
+make
+cd src
+
+if [ ! -x gomobile ]
+then
+  go get golang.org/x/mobile/cmd/gomobile
+  gomobile init
+fi
+
+GOPATH=`pwd`/.. go get gopkg.in/yaml.v1
+GOPATH=`pwd`/.. go get github.com/alecthomas/log4go
+GOPATH=`pwd`/.. go get github.com/rcrowley/go-metrics
+GOPATH=`pwd`/.. go get github.com/inconshreveable/go-vhost
+GOPATH=`pwd`/.. go get github.com/nsf/termbox-go
+
+GOPATH=`pwd`/.. go get golang.org/x/mobile/cmd/gomobile
+
 gomobile clean
  GOPATH=`pwd`/.. gomobile bind -v -target ios -tags debug -gcflags=" -E -K -j -l -r -v -w" ngrok/client
  GOPATH=`pwd`/.. gomobile bind -v -target ios -tags debug -gcflags=" -E -K -j -l -r -v -w" ngrok/server
